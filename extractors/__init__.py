2f7fdfd59bc80b8af723bcf020b1ce6837e9c898
piyushtyagi <piyushtyagi@gofynd.com>
2025-05-14 17:16:00 +0530

    extractors added to init file

---
+++ extractors/__init__.py
@@ -0,0 +1,8 @@
+from extractors.extractor import (
+    DataExtractor,
+    GitRepoExtractor,
+    GitHubRepoExtractor,
+    AzureDevopsRepoExtractor,
+    GitLabRepoExtractor,
+    QuipExtractor,
+)
